a5141bd84569b38472594e9962bd8b66a9f902a1
emmanuel Mensah <emmaxp183@gmail.com>
2022-03-08 16:20:43 +0100

    Initial Commit

---
+++ multiverse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  multiverse
+//
+//  Created by admin on 08/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ multiverse/multiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiverseApp.swift
+//  multiverse
+//
+//  Created by admin on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct multiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
