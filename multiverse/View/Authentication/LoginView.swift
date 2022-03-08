d8b5745b07e852766bfe82a95152f564e710a4b4
emmanuel Mensah <emmaxp183@gmail.com>
2022-03-08 23:54:23 +0100

    ///BUILDING LOG IN SCREEN UI

--- multiverse/View/Authentication/LoginView.swift
+++ multiverse/View/Authentication/LoginView.swift
@@ -19,34 +19,88 @@ struct LoginView: View {
         
         
         
-        VStack(alignment: .leading,spacing:0.5) {
-            Image("image1")
-                .resizable()
-                .scaledToFit()
-           
-            Text("Welcome To")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-            Text("Multiverse")
-                .font(.system(size: 35, weight: .heavy, design: .rounded)).foregroundColor(Color(red: 0.295, green: 0.428, blue: 0.395))
-           
-            
-            //TEXFIELD & SECURED FIELD
-            
-            VStack{
+        NavigationView {
+            VStack {
+                VStack(alignment: .leading,spacing:3) {
+                    Image("image1")
+                        .resizable()
+                        .scaledToFit()
+                   
+                    Text("Welcome To")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                    Text("Multiverse")
+                        .font(.system(size: 35, weight: .heavy, design: .rounded)).foregroundColor(Color(red: 0.295, green: 0.428, blue: 0.395))
+                   
+                    
+                    //TEXFIELD & SECURED FIELD
+                    
+                    VStack{
+                        
+                        TextField("Email",text: $email )
+                        SecureField("Password", text: $password)
+                        
+                        
+                        
+                    }.padding([.top,.horizontal],32)
+                    
+                    ///END
+                    
+                    
+                    
+                    Spacer()
+                }.padding()
                 
-                TextField("Email",text: $email )
-                SecureField("Password", text: $password)
+               
+                
+                // RESET PASSWORD
+                
+                NavigationLink(destination: Text("Reset password")) {
+                    
+                    HStack{
+                        Spacer()
+                        Text("Reset password")
+                            .fontWeight(.heavy)
+                            .font(.subheadline)
+                            .foregroundColor(Color(red: 0.309, green: 0.448, blue: 0.411))
+                        
+                    }.padding(.horizontal,32)
+               
+                } //END
                 
                 
                 
-            }.padding()
-            
-            
-            
-            
-            
-            Spacer()
-        }.padding()
+                
+                ///BUTTON
+                
+                Button(action:{}){
+                    
+                    Text("Sign In").fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 50)
+                        .background(Color(red: 0.309, green: 0.448, blue: 0.411))
+                        .cornerRadius(13)
+                }
+               /// END OF BUTTON
+                
+                //SIGN UP
+                NavigationLink(destination: RegistrationView()) {
+                    
+                    HStack{
+                       
+                        Text("Dont Have Account!").font(.subheadline)
+                            .foregroundColor(.black)
+                        Text("SignUp").fontWeight(.heavy).foregroundColor(Color(red: 0.309, green: 0.448, blue: 0.411))
+                        
+                    }.padding(.horizontal,30)
+                
+                
+                
+            }//END
+                
+                
+                
+                
+        }.padding(.top,-79)
     }
 }
 
@@ -54,4 +108,5 @@ struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
+}
 }
