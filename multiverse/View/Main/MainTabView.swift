23c60e34c23d7d0a55f7effc8561c30bf0367db7
emmanuel Mensah <emmaxp183@gmail.com>
2022-03-11 19:46:08 +0100

    ///BUILDING SETTINGS  VIEW

--- multiverse/View/Main/MainTabView.swift
+++ multiverse/View/Main/MainTabView.swift
@@ -8,38 +8,73 @@
 import SwiftUI
 
 struct MainTabView: View {
+    // PROPERTIES
+    @State private var SelectedIndex = 0
+    
     var body: some View {
-        TabView {
-            ChatView()
+        NavigationView {
+            TabView (selection : $SelectedIndex){
+                ChatView()
+                    .onTapGesture{
+                        SelectedIndex = 0
+                    }
+                    .tabItem {
+                        Image(systemName: "ellipsis.bubble")
+                        
+                    }
+                    .tag(0)
+                ChannelView()
+                    .onTapGesture{
+                        SelectedIndex = 1
+                    }
+                    .tabItem {
+                        Image(systemName: "person.3")
+                     
+                    }
+                    .tag(1)
+                NewFeedView()
+                    .onTapGesture{
+                        SelectedIndex = 2
+                    }
+                    .tabItem {
+                        Image(systemName: "list.bullet.rectangle")
+                       
+                    }
+                    .tag(2)
+           
+            SettingsView()
+                    .onTapGesture{
+                        SelectedIndex = 3
+                    }
                 .tabItem {
-                    Image(systemName: "ellipsis.bubble")
-                    Text("Chat")
-                }
-            ChannelView()
-                .tabItem {
-                    Image(systemName: "person.3")
-                    Text("Channel")
-                }
+                    Image(systemName: "gearshape")
+                    
+                
+            }
+                .tag(3)
+            }
             
-            NewFeedView()
-                .tabItem {
-                    Image(systemName: "list.bullet.rectangle")
-                    Text("News Feed")
-                }
-       
-        SettingsView()
-            .tabItem {
-                Image(systemName: "gearshape")
-                Text("Settings")
-            
-        }
+                .navigationTitle(tabTitle)
         }
 }
+    var tabTitle : String {
+        switch SelectedIndex {
+        case 0 : return "Chats"
+        case 1 : return "Channel"
+        case 2 : return "News Feed"
+        case 3 : return "Settings"
+            
+            
+        default: return ""
+            
+        }
+        
+    }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
     }
 }
 }
